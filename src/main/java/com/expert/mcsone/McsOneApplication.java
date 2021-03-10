7b6871f0d3d2da17d5ca585a72bcc244729c87a9
jchaconv <jcaaron.chacon@outlook.com>
2021-03-09 21:51:40 -0500

    Added files to the repo

---
+++ src/main/java/com/expert/mcsone/McsOneApplication.java
@@ -0,0 +1,13 @@
+package com.expert.mcsone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expert/mcsone/McsOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expert.mcsone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
